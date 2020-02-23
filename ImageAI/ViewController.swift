fb823c1fb7b71ece08afabdcdd561f0f7d34d38b
Atsushi Adachi <atsuk0r05296@gmail.com>
2020-02-23 11:16:40 +0900

    Initial Commit

---
+++ ImageAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageAI
+//
+//  Created by 安達篤史 on 2020/02/23.
+//  Copyright © 2020 安達篤史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
